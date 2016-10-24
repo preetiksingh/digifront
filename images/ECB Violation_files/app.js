af276c10fbc097c3358cad48b672cc81a24284f4
preetiksingh <preetisingh41451@gmail.com>
2016-10-24 18:46:36 +0530

    Modification in secondary nav bar

---
+++ images/ECB Violation_files/app.js
@@ -0,0 +1,23 @@
+$( document ).ready(function() {
+  $('.second-nav').affix({
+    offset: {
+      top: 760
+    }
+  });
+
+  $(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
+  });
+
+});
