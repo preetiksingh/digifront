b5ee4b60a0fad17284c5a194c44e065d22f60692
preetiksingh <preetisingh41451@gmail.com>
2016-11-07 00:21:03 +0530

    Added smooth scroll for glossary

--- js/app.js
+++ js/app.js
@@ -54,4 +54,16 @@ $(document ).ready(function() {
       }
     });
   });
+
+$("#scroll-target ul li a[href^='#']").on('click', function(e) {
+ e.preventDefault();
+ var hash = this.hash;
+ $('#scroll-content').animate({
+     scrollTop: $(hash).offset().top
+   }, 400, function(){
+     window.location.hash = hash;
+   });
+
+});
+
 });
