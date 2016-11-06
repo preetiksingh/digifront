8040b00266a2fbef022ebf1cddaf93b9c8e79171
preetiksingh <preetisingh41451@gmail.com>
2016-11-06 12:19:44 +0530

    Added one more dummy content to glossary

--- js/app.js
+++ js/app.js
@@ -41,7 +41,7 @@ $(document ).ready(function() {
 
 
   $(function() {
-    $('a[href*="#"]:not([href="#"])').click(function() {
+    $('#secondary-nav a[href*="#"]:not([href="#"])').click(function() {
       if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
         var target = $(this.hash);
         target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
